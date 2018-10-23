5cb08a36eb2ebcefb3129f2da2b81f4e3e9a6886
Nathan Yaw Oppong Donkor <gerrard8donkor@gmail.com>
2018-10-23 10:34:38 +0000

    Solution to Breadth first search--java

---
+++ Java/Graph.java
@@ -0,0 +1,45 @@
+public static class Graph {
+    //List of list of integers to record the nodes
+    List <List<Integer>> adjacents;
+    int size;
+
+    public Graph(int size) {
+        //initializing everything here in the constractor.
+        this.size= size;
+        adjacents = new ArrayList<>();
+        while(size-- >0){
+            adjacents.add(new ArrayList<Integer>());
+        }
+    }
+
+    public void addEdge(int first, int second) {
+        adjacents.get(first).add(second);
+        adjacents.get(second).add(first);
+    }
+
+    public int[] shortestReach(int startId) { // 0 indexed
+
+        int[] distances = new int[size];
+        Arrays.fill(distances, -1);
+        Queue<Integer> que = new LinkedList<>();
+
+        que.add(startId);
+        distances[startId] = 0;
+        HashSet<Integer> visited = new HashSet<>();
+
+        visited.add(startId);
+        while(!que.isEmpty()) {
+            Integer curr = que.poll();
+            for(int node : adjacents.get(curr)) {
+                if(!visited.contains(node)) {
+                    que.offer(node);
+        // Right place to add the visited set.
+                    visited.add(node);
+        // keep on increasing distance level by level.
+                    distances[node] = distances[curr] + 6;
+                }
+            }
+        }
+        return distances;
+    }
+}
